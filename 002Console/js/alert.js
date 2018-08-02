b2c714341835e86a053ac0fda8be8ed57c4c72d7
DELL <1>
2018-08-03 07:44:34 +0800

    a

---
+++ 002Console/js/alert.js
@@ -0,0 +1,14 @@
+function taoLu() {
+    alert("你猜一会会发生什么");
+    alert("再等等");
+    alert("也许真有奇迹");
+}
+alert("你怎么打开了这个网页");
+taoLu();//调用function
+alert("你相信奇迹吗");
+alert("我来向你提一个问题");
+taoLu();//调用function
+alert("果然没有奇迹");
+alert("我们就这样放弃吗");
+taoLu();//调用function
+alert("最后一句了");
